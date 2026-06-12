f0fe47ffa2b3a7a9328ec6cc45d2ff7896fa7f46
Sedcer <129540947+Sedcer@users.noreply.github.com>
2026-06-12 12:32:37 +0200

    Update Room.js

--- src/Room.js
+++ src/Room.js
@@ -10,6 +10,11 @@
  * @typedef {string|number} Value A value stored in a variable in a Room.
  */
 console.log(process.env.DATABASE_URL);
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+console.log("PGHOST:", process.env.PGHOST);
+console.log("PGPORT:", process.env.PGPORT);
+console.log("PGUSER:", process.env.PGUSER);
+console.log("PGDATABASE:", process.env.PGDATABASE);
 
 const { Pool } = require('pg');
 
